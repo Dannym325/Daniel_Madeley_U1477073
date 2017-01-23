6b516a60569527080870eeadf37480a01e98b2df
Danny Madeley <u1477073@unimail.hud.ac.uk>
2017-01-23 12:51:49 +0000

    Added a userService and UserRepo files so that i can start mkaing the log in work with the H2 database.

---
+++ src/main/java/com/blog/UserRepository.java
@@ -0,0 +1,16 @@
+package com.blog;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by Danny Madeley on 23/01/17.
+ */
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
+
+
+
+
+
+
---
+++ src/main/java/com/blog/UserService.java
@@ -0,0 +1,30 @@
+package com.blog;
+
+import com.blog.User;
+import com.blog.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by Danny Madeley on 23/01/17.
+ */
+@Service
+public class UserService {
+    @Autowired
+    protected UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public void delete(User user) {
+        userRepository.delete(user);
+    }
+}
+
